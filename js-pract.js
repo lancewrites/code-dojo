ceff634c7a5122193ef9f9fcefeb552098df9c72
lancewrites <lancewrites77@gmail.com>
2022-05-12 19:05:36 -0700

    first batch

---
+++ js-pract.js
@@ -0,0 +1,28 @@
+// Write the code that will log the outcome of 34 added to 71.
+ 
+// console.log()
+// Write the code that will log the outcome of 67 subtracted from 123.
+//  console.log(123-67)
+// Write the code that will log the outcome of 56 multiplied by 23.
+//  console.log(56*23)
+// Write the code that will log the outcome of 45 divided by 5.
+//  console.log(45/5)
+// Write the code that will log the outcome of 5 to the power of 7.
+//  console.log(5^7)
+// Write the code that will log the whole number remainder of 33 divided by 6.
+// console.log(33/6)
+// Write the code that will log the length of a string containing your name.
+var myName = "Lance"
+console.log(myName.length)
+// Write the code that will log whether your string includes the letter "e"?
+console.log(myName.includes("e"))
+// Write the code that will log the character at the first index of the string.
+console.log(myName.charAt(0))
+console.log(myName[0])
+// Write the code that will log the string in all uppercase letters.
+console.log(myName.toUpperCase())
+
+// Create the code that will log the number of times the letter "e" occurs in the string.
+
+
+// Create the code that will log every other character in the string.
